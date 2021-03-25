ad43c28ddccca442ea972b25182e2c08beb7cd6b
Daniil Subbotin <d.subbotin@opends.tech>
2021-03-25 23:15:11 +0300

    Add ability to use custom RequestAdapter with URLSessionClient
    Closes #19

--- Tests/ApexyAlamofireTests/BaseRequestInterceptorTests.swift
+++ Tests/ApexyAlamofireTests/BaseRequestInterceptorTests.swift
@@ -14,11 +14,11 @@ final class BaseRequestInterceptorTests: XCTestCase {
     
     private let url = URL(string: "https://booklibrary.com")!
     
-    var interceptor: RequestInterceptor {
+    private var interceptor: RequestInterceptor {
         BaseRequestInterceptor(baseURL: url)
     }
     
-    func testAdapt() {
+    func testAdaptWhenURLNotContainsTrailingSlash() {
         let request = URLRequest(url: URL(string: "books/10")!)
         
         let expectation = XCTestExpectation(description: "Wait for completion")
@@ -34,7 +34,7 @@ final class BaseRequestInterceptorTests: XCTestCase {
         wait(for: [expectation], timeout: 1)
     }
     
-    func testAdapt_urlPathWithTrailingSlash() {
+    func testAdaptWhenURLContainsTrailingSlash() {
         let request = URLRequest(url: URL(string: "path/")!)
         let exp = expectation(description: "Adapting url request")
         interceptor.adapt(request, for: .default) { result in
@@ -45,18 +45,7 @@ final class BaseRequestInterceptorTests: XCTestCase {
         wait(for: [exp], timeout: 1)
     }
     
-    func testAdapt_urlPathWithoutTrailingSlash() {
-        let request = URLRequest(url: URL(string: "path")!)
-        let exp = expectation(description: "Adapting url request")
-        interceptor.adapt(request, for: .default) { result in
-            let request = try! result.get()
-            XCTAssertEqual(request.url?.absoluteString, "https://booklibrary.com/path")
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1)
-    }
-    
-    func testAdapt_urlPathWithTrailingSlashWithQuery() {
+    func testAdaptWhenURLContainsQueryItems() {
         let url = URL(string: "api/path/")!
         var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
         components.queryItems = [URLQueryItem(name: "param", value: "value")]
@@ -69,5 +58,22 @@ final class BaseRequestInterceptorTests: XCTestCase {
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
+    }
+    
+    func testAdaptWhenRequestContainsHeaders() {
+        var request = URLRequest(url: URL(string: "books")!)
+        request.addValue("application/octet-stream", forHTTPHeaderField: "Content-Type")
+        
+        let expectation = XCTestExpectation(description: "Wait for completion")
+        interceptor.adapt(request, for: .default) { result in
+            switch result {
+            case .success(let req):
+                XCTAssertEqual(req.value(forHTTPHeaderField: "Content-Type"), "application/octet-stream")
+            case .failure:
+                XCTFail("Expected result: .success, actual result: .failure")
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
     }
 }
